d7c199a49eabc3f97a21c3710563a32141910fbe
Viktor Michalek <viktor.michalek@seznam.cz>
2022-09-16 21:19:06 +0200

    models.py: snad finalni model

--- kb/models.py
+++ kb/models.py
@@ -3,28 +3,48 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 
-# Create your models here.
 class Category(models.Model):
-    name = models.CharField('Category name', max_length=64)
+    name = models.CharField('Category name', max_length=32, unique=True)
     description = models.CharField('Description', max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        full_path = [self.name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1])
 
 
 class Tag(models.Model):
-    name = models.CharField('Tag name', max_length=64)
-    slug = models.SlugField()
+    name = models.CharField('Tag name', max_length=64, unique=True)
+    slug = models.SlugField(unique=True)
 
 
 class Article(models.Model):
     # id_article = models.AutoField()
-    name = models.CharField('Article name', max_length=128)
+    title = models.CharField('Article name', max_length=128, unique=True)
     description = models.CharField('Description', max_length=255)
     content = HTMLField()
     is_public = models.BooleanField('Is public', default=False)
-    created = models.DateTimeField('Creation date and time')
-    slug = models.SlugField()
-    publish = models.DateField(auto_now=False, auto_now_add=False,)
+    slug = models.SlugField(unique=True)
+    created = models.DateTimeField('Creation date and time', auto_now=False, auto_now_add=False)
+    publish = models.DateTimeField('Publication date and time', auto_now=False, auto_now_add=False)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     tag = models.ManyToManyField('Tag')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_cat_list(self):
+        k = self.category  # for now ignore this instance method
+        breadcrumb = ["dummy"]
+        while k is not None:
+            breadcrumb.append(k.slug)
+            k = k.parent
+        for i in range(len(breadcrumb)-1):
+            breadcrumb[i] = '/'.join(breadcrumb[-1:i-1:-1])
+        return breadcrumb[-1:0:-1]
