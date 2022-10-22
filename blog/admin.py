fbe671d029f194e717d7297e3659b9636173f97c
Viktor Michalek <viktor.michalek@seznam.cz>
2022-10-23 00:05:30 +0200

    Category tree DraggableMPTTAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Category, Tag, Post
+from mptt.admin import DraggableMPTTAdmin
 
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(DraggableMPTTAdmin):
     search_fields = ['name', 'description']
     prepopulated_fields = {'slug': ('name',)}
 
