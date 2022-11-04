1927b6057a8197a8b4a07b4f7bafa3bbd623db4f
Viktor Michalek <viktor.michalek@seznam.cz>
2022-11-04 21:52:58 +0100

    django-simple-captcha installation

--- vimsblog/settings.py
+++ vimsblog/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tinymce',
     'mptt',
+    'captcha',
     'blog',
 ]
 
--- vimsblog/urls.py
+++ vimsblog/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
+    path('captcha/', include('captcha.urls')),
     path('', include('blog.urls'))
 ]
